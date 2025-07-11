e4674e2c4873b2631a90782ee392cc0b7597d453
chiedozieu <chiedozieugwor@gmail.com>
2025-07-11 08:29:57 -0400

    feat: add initial page components and Navbar
    - Created Navbar component with logo and "Get Started" button.
    - Added BlogTitles, Community, Dashboard, GenerateImages, Home, Layout, RemoveBackground, RemoveObject, ReviewResume, and WriteArticle components with basic structure.

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
+import { ArrowBigRight } from "lucide-react";
+
+const Navbar = () => {
+  
+  return (
+    <div className="fixed w-full z-5 backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32">
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="w-32 sm:w-40" />
+      </Link>
+      <button className="flex items-center gap-2 rounded-full text-white bg-gradient-to-r from-[#12B7AC] via-cyan-600.5 to-[#08B6CE] py-2 px-4 cursor-pointer">Get Started <ArrowBigRight className="size-4"/> </button>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ client/src/pages/BlogTitles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogTitles = () => {
+  return (
+    <div className='mt-20'>BlogTitles</div>
+  )
+}
+
+export default BlogTitles
